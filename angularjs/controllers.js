decb88f17f9d54b24426a9b4773d3b34e6d59b6a
chisun-joung <csjoung@gmail.com>
2016-08-07 14:16:44 +0900

    Scope child controllers

--- angularjs/controllers.js
+++ angularjs/controllers.js
@@ -1,9 +1,38 @@
-var controllersModule = angular.module("exampleApp.Controllers",[])
+var app = angular.module("exampleApp",[])
 
-controllersModule.controller("dayCtrl", function ($scope,days) {
-    $scope.day = days.today;
+app.controller("topLevelCtrl", function ($scope) {
+    $scope.dataValue = "Hello, Adam";
+
+    $scope.reverseText = function() {
+        $scope.dataValue = $scope.dataValue.split("").reverse().join("");
+    }
+
+    $scope.changeCase = function() {
+        var result = [];
+        angular.forEach($scope.dataValue.split(""), function(char, index){
+            result.push(index % 2 == 1 
+                ? char.toString().toUpperCase() : char.toString().toLowerCase());
+        });
+        $scope.dataValue = result.join("");
+    };
 });
 
-controllersModule.controller("tomorrowCtrl", function($scope, days) {
-    $scope.day = days.tomorrow;
+app.controller("firstChildCtrl", function($scope) {
+    $scope.changeCase = function() {
+        $scope.dataValue = $scope.dataValue.toUpperCase();
+    };
+});
+
+app.controller("secondChildCtrl", function($scope) {
+    $scope.changeCase = function() {
+        $scope.dataValue = $scope.dataValue.toLowerCase();
+    };
+
+    $scope.shiftFour = function() {
+        var result = [];
+        angular.forEach($scope.dataValue.split(""), function(char, index) {
+            result.push(index < 4 ? char.toUpperCase() : char);
+        });
+        $scope.dataValue = result.join("");
+    }
 });
