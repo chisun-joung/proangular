ba54afb1fa4aa7dc0014a5f9ebedaeb8965365de
chisun-joung <csjoung@gmail.com>
2016-09-23 17:08:05 +0900

    products list

--- angularjs/products.js
+++ angularjs/products.js
@@ -2,53 +2,57 @@
  * Created by csjoung on 2016. 9. 23..
  */
 angular.module("exampleApp",[])
-.controller("defaultCtrl", function($scope){
-    $scope.displayMode = "list";
-    $scope.currentProcut = null;
-
-    $scope.listProducts = function() {
-        $scope.products = [
-            {id: 0, name: "Dummy1", category: "Test", price: 1.25},
-            {id: 1, name: "Dummy2", category: "Test", price: 2.45},
-            {id: 3, name: "Dummy3", category: "Test", price: 4.25}];
-    }
-
-    $scope.deleteProduct = function (product) {
-        $scope.products.splice($scope.products.indexOf(product), 1);
-    }
-
-    $scope.createProduct = function (product) {
-        $scope.products.push(product);
+    .constant("baseUrl", "http://localhost:5500/products/")
+    .controller("defaultCtrl", function($scope, $http, baseUrl){
         $scope.displayMode = "list";
-    }
+        $scope.currentProduct = null;
 
-    $scope.updateProduct = function (product) {
-        for(var i = 0; i < $scope.products.length; i++) {
-            if($scope.products[i].id == products.id) {
-                $scope.products[i] = product;
-                break;
+        $scope.listProducts = function() {
+            /*$scope.products = [
+                {id: 0, name: "Dummy1", category: "Test", price: 1.25},
+                {id: 1, name: "Dummy2", category: "Test", price: 2.45},
+                {id: 3, name: "Dummy3", category: "Test", price: 4.25}];*/
+            $http.get(baseUrl).success(function (data) {
+                $scope.products = data;
+            })
+        }
+
+        $scope.deleteProduct = function (product) {
+            $scope.products.splice($scope.products.indexOf(product), 1);
+        }
+
+        $scope.createProduct = function (product) {
+            $scope.products.push(product);
+            $scope.displayMode = "list";
+        }
+
+        $scope.updateProduct = function (product) {
+            for(var i = 0; i < $scope.products.length; i++) {
+                if($scope.products[i].id == product.id) {
+                    $scope.products[i] = product;
+                    break;
+                }
+            }
+            $scope.displayMode = "list";
+        }
+
+        $scope.editOrCreateProduct = function (product) {
+            $scope.currentProduct = product ? angular.copy(product) : {} ;
+            $scope.displayMode = "edit";
+        }
+
+        $scope.saveEdit = function (product) {
+            if(angular.isDefined(product.id)){
+                $scope.updateProduct(product);
+            } else {
+                $scope.createProduct(product);
             }
         }
-        $scope.displayMode = "list";
-    }
 
-    $scope.editOrCreateProduct = function (product) {
-        $scope.currentProcut = product ? angular.copy(product) : {} ;
-        $scope.displayMode = "edit";
-    }
-
-    $scope.saveEdit = function (product) {
-        if(angular.isDefined(product.id)){
-            $scope.updateProduct(product);
-        } else {
-            $scope.createProduct(product);
+        $scope.cancelEdit = function () {
+            $scope.currentProduct = {};
+            $scope.displayMode = "list";
         }
-    }
 
-    $scope.cancelEdit = function () {
-        $scope.currentProduct = {};
-        $scope.displayMode = "list";
-    }
-
-    $scope.listProducts();
-});
+        $scope.listProducts();
+    });
