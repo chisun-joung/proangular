e049d1034afd7d48835aa6ba36e989252d1a82e3
chisun-joung <csjoung@gmail.com>
2016-10-05 05:32:12 +0900

    fix karma.config.js

--- angularjs/karma.config.js
+++ angularjs/karma.config.js
@@ -16,7 +16,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'angular.js',
-      'angular-mock.js',
       'angular-mocks.js',
       '*.js',
       'tests/*.js'
